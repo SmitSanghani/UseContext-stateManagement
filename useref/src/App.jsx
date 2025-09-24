ec467f98d04082c5ac5b023be15e30ee16ae8b8b
Smit <smitbcastudent@gmail.com>
2025-09-24 11:59:31 +0530

    update

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import UseRef from './component/UseRef'
+import UseRef2 from './component/UseRef2'
+import UseRefAnimationExample from './component/UseRefAnimationExample'
 
 const App = () => {
   return (
     <div>App
       <UseRef/>
+      <UseRef2/>
+      <UseRefAnimationExample/>
       </div>
   )
 }
